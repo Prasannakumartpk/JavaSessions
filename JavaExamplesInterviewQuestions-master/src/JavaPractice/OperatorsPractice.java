3c064ae1a5b7078c036f9ea7781866c2c6faef6f
Prasannakumar T <prasanna.thallapally@acuvate.com>
2024-01-04 12:08:15 +0530

    Operators Class added

---
+++ JavaExamplesInterviewQuestions-master/src/JavaPractice/OperatorsPractice.java
@@ -0,0 +1,19 @@
+package JavaPractice;
+
+public class OperatorsPractice {
+
+	public static void main(String[] args) {
+		  
+		
+		int i = 30;
+		int j = 40;
+		
+		System.out.println(i+j);
+		System.out.println(i-j);
+		System.out.println(i*j);
+		System.out.println(i/j);
+		System.out.println(i%j);
+
+	}
+
+}
