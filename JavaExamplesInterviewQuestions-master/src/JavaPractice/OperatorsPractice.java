e11a7eac93c6582285c282fc4d538d54bb6d5eb8
Prasannakumar T <prasanna.thallapally@acuvate.com>
2024-01-04 12:21:07 +0530

    Unary Operators code added and commented

--- JavaExamplesInterviewQuestions-master/src/JavaPractice/OperatorsPractice.java
+++ JavaExamplesInterviewQuestions-master/src/JavaPractice/OperatorsPractice.java
@@ -8,11 +8,26 @@ public class OperatorsPractice {
 		int i = 30;
 		int j = 40;
 		
-		System.out.println(i+j);
-		System.out.println(i-j);
-		System.out.println(i*j);
-		System.out.println(i/j);
-		System.out.println(i%j);
+		/* 1. Arithmetic Operators
+		 * System.out.println(i+j); 
+		 * System.out.println(i-j); 
+		 * System.out.println(i*j);
+		 * System.out.println(i/j); 
+		 * System.out.println(i%j);
+		 */
+		
+		
+		/* 2. Uanry Operators
+		 * System.out.println(i); 
+		 * System.out.println(++i); 
+		 * System.out.println(i++);
+		 * System.out.println(i);
+		 * 
+		 * System.out.println(i); 
+		 * System.out.println(--i); 
+		 * System.out.println(i--);
+		 * System.out.println(i);
+		 */
 
 	}
 
