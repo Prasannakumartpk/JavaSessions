f374ac4c6c29f326fe6f86aeb80e40120763e909
Prasannakumartpk <prasannakumartpk@gmail.com>
2025-01-18 18:39:27 +0530

    update

---
+++ JavaExamplesInterviewQuestions-master/src/JavaPractice/ConditionalStatements.java
@@ -0,0 +1,22 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		/*
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Given number is");
+		int i = scanner.nextInt();
+		
+		if(i % 2 == 0) {
+			System.out.println(i + " is even");
+		}else {
+			System.out.println(i + " is odd");
+		}
+		*/
+
+	}
+
+}
